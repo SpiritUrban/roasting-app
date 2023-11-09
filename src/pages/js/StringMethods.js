c276ee8290a3be9aa418f48047dc0b2e5416237a
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2023-11-09 17:51:10 +0200

    fix; clean up;

--- src/pages/js/StringMethods.js
+++ src/pages/js/StringMethods.js
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import yaml from 'js-yaml';
-import { log, loadYaml } from 'utils';
+import { loadYaml } from 'utils';
 import Teacher from 'features/Teacher';
-
-
 
 const StringMethods = () => {
   const [data, setData] = useState(null);
 
-  // Эффект для отслеживания монтирования компонента
   useEffect(() => {
-    // Немедленно вызываемая асинхронная функция (IIFE)
     (async () => {
       try {
         const yamlData = await loadYaml('/data/js/StringMethods.yaml');
@@ -23,16 +17,14 @@ const StringMethods = () => {
     return () => {
       console.log("Компонент Counter будет размонтирован.");
     };
-  }, []); // Пустой массив зависимостей означает, что эффект выполнится один раз после первого рендера.
-
+  }, []);
 
   return (
     <div>
       {data ? (
         <div>
-          <h2>Пользователи:</h2>
-          
-          <Teacher data={data}/>
+          <h2>JavaScript String Methods</h2>
+          <Teacher data={data} />
         </div>
       ) : (
         <p>Loading...</p>
